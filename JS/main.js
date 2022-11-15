5976fb61709f423691b8f365174d3517112102ca
GHOST-Aram <owinoojala@gmail.com>
2022-11-15 22:14:48 +0300

    Create Keys and add audiofiles

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+// const keys = document.querySelectorAll('.key')
+// const sounds = document.querySelectorAll('audio')
+// const dataKeys = document.querySelectorAll('data-key')
+
+// window.addEventListener('keydown', function(e){
+//     const audio = this.document.querySelector(`audio[data-key='${e.key.toUpperCase()}']`)
+//     if(!audio)return//stops execution if key is not associated with any audio
+//     console.log(audio)
+//     audio.play()//playAudio
+// })
